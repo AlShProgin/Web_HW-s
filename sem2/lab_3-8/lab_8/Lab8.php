a73dd3f2876c17bae9a412e5c9f4e7d6d2bd7802
AlShProgin <90740317+AlShProgin@users.noreply.github.com>
2022-06-25 18:22:29 +1000

    Update Lab8.php

--- sem2/lab_3-8/lab_8/Lab8.php
+++ sem2/lab_3-8/lab_8/Lab8.php
@@ -85,40 +85,4 @@ switch ($activeButton){
     default:
         echo "Unexpected result";
 }
-
-
 echo $twig->render('lab8_formMode.twig');
-
-/*
-try{
-    $db = new PDO('mysql:host=localhost;dbname=testDB', 'sasha', 'rootroot');
-    $result = $db->query('SELECT * FROM users');
-    $rows = $result->fetchAll(PDO::FETCH_ASSOC);
-    echo "Already existing users:<br>";
-    foreach($rows as $row){
-    	$uname = $row['username'];
-        echo "$uname<br>";
-    }
-    $loader = new FilesystemLoader(__DIR__ . "/templates");
-    $twig = new Environment($loader);
-    echo $twig->render('lab7_form.twig');
-    if(isset($_GET["inputName"]) && isset($_GET["inputPassword"])) {
-        $uname = $_GET["inputName"];
-        $upassword = $_GET["inputPassword"];
-
-        $insert_query = $db->prepare("INSERT INTO users (username, password) VALUES (\"$uname\",\"$upassword\")");
-        $insert_query->execute();
-        echo $twig->render('lab7_newUser.twig', ['username' => $uname, 'password' => $upassword]);
-    }
-    
-} catch (PDOException $e) {
-  print "<br>Error!: " . $e->getMessage();
-  die();
-} catch (Exception $ex) {
-  print "<br>Error!: " . $ex->getMessage();
-  die();
-}
-
-$dbh = null;
-$stmt = null;
-*/
